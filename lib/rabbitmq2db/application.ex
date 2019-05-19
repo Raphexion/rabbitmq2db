81709d4f3675d0dde9df933cf5c1a3458f509d09
Niklas Johansson <raphexion@gmail.com>
2019-05-19 12:11:06 +0200

    Adjusts application to support phoenix

--- lib/rabbitmq2db/application.ex
+++ lib/rabbitmq2db/application.ex
@@ -8,14 +8,27 @@ defmodule RabbitMQ2DB.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {RabbitMQ2DB.Repo, []},
       {RabbitMQ2DB.DataReceiver, []},
-      {RabbitMQ2DB.InfoReceiver, []}
+      {RabbitMQ2DB.InfoReceiver, []},
+
+      # Start the Ecto repository
+      RabbitMQ2DB.Repo,
+      # Start the endpoint when the application starts
+      RabbitMQ2DBWeb.Endpoint
+      # Starts a worker by calling: RabbitMQ2DB.Worker.start_link(arg)
+      # {RabbitMQ2DB.Worker, arg},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: RabbitMQ2DB.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    RabbitMQ2DBWeb.Endpoint.config_change(changed, removed)
+    :ok
   end
 end
